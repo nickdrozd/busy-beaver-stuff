2a89a890f176b15b7c7c274b534806c3e9566cc9
Nick Drozd <nicholasdrozd@gmail.com>
2020-07-31 11:43:41 -0500

    Comment out BB5 for now

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ BB2 = "1RB   1LB   1LA   1RH"
 BB3 = "1RB   1RH   1LB   0RC   1LC   1LA"
 BB4 = "1RB   1LB   1LA   0LC   1RH   1LD   1RD   0RA"
 TM5 = "1RB   0LC   1RC   1RD   1LA   0RB   0RE   1RH   1LC   1RA"
-
 BB5 = "1RB   1LC   1RC   1RB   1RD   0LE   1LA   1LD   1RH   0LA"
 
 
@@ -16,7 +15,7 @@ EXPECTED = {
     BB3: (5, 21),
     BB4: (13, 107),
     TM5: (501, 134467),
-    BB5: (4098, 47176870),
+    # BB5: (4098, 47176870),
 }
 
 
