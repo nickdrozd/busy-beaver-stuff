c1d443d76476924df9937e0a2c9f9e4a9ac94dbe
Nick Drozd <nicholasdrozd@gmail.com>
2020-07-30 23:04:50 -0500

    Better parse format

--- turing.py
+++ turing.py
@@ -1,18 +1,20 @@
 STATE_MAP = {
-    'A': '1',
-    'B': '2',
-    'C': '3',
-    'D': '4',
-    'E': '5',
-    'F': '6',
-    'G': '7',
-    'H': '0',
+    'A': '0',
+    'B': '1',
+    'C': '2',
+    'D': '3',
+    'E': '4',
+    'F': '5',
+    'G': '6',
+    'H': '7',
 }
 
 
 def parse(instructions):
     instructions = iter([
-        instr[1:]
+        instr[1:].replace(
+            instr[2],
+            STATE_MAP[instr[2]])
         for instr in
         instructions.split()
     ])
