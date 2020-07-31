d4a1750fead54de6fb224b49576cd68672e29daa
Nick Drozd <nicholasdrozd@gmail.com>
2020-07-31 11:19:51 -0500

    Rework main

--- turing.py
+++ turing.py
@@ -16,7 +16,6 @@ SHIFT_MAP = {
     'R': 1,
 }
 
-
 def parse(program_string):
     instructions = iter([
         instr.replace(
@@ -34,9 +33,7 @@ def parse(program_string):
         zip(instructions, instructions)
     )
 
-
-BB3 = "1RB   1RH   1LB   0RC   1LC   1LA"
-
+########################################
 
 class Machine:
     def __init__(self, tape, prog):
@@ -89,6 +86,7 @@ class Machine:
     def run_to_halt(self):
         while self._state != HALT:
             self.exec()
+            print(self._exec_count)
 
     def print_results(self):
         squares = [
@@ -107,12 +105,19 @@ class Machine:
                 sum(self._tape),
                 with_pos))
 
+########################################
+
+BB3 = "1RB   1RH   1LB   0RC   1LC   1LA"
+BB4 = "1RB   1LB   1LA   0LC   1RH   1LD   1RD   0RA"
+TM5 = "1RB   0LC   1RC   1RD   1LA   0RB   0RE   1RH   1LC   1RA"
+BB5 = "1RB   1LC   1RC   1RB   1RD   0LE   1LA   1LD   1RH   0LA"
+
+
+def run_bb(prog):
+    machine = Machine([0], prog)
+    machine.run_to_halt()
+    machine.print_results()
+
 
 if __name__ == '__main__':
-    bb3 = Machine([0], BB3)
-    bb3.run_to_halt()
-    bb3.print_results()
-
-    bb4 = Machine([0], '1RB   1LB   1LA   0LC   1RH   1LD   1RD   0RA')
-    bb4.run_to_halt()
-    bb4.print_results()
+    run_bb(TM5)
