68ea853b5ce99b39e92976dd6f08bb1b6892c383
Nick Drozd <nicholasdrozd@gmail.com>
2020-08-18 16:15:14 -0500

    Add parse_graph.py

--- Makefile
+++ Makefile
@@ -20,3 +20,7 @@ clean :
 gen3 :
 	python3 generate-3-state.py > 3-state-programs.txt
 	wc -l 3-state-programs.txt
+
+# make parse_graph prog="'1LB 1LC 1RC 1LD 1LA 1RD 0LD 0RB'"
+parse_graph :
+	python3 parse_graph.py $(prog) | dot -Tpng -o out.png
---
+++ parse_graph.py
@@ -0,0 +1,36 @@
+import sys
+
+STATES = ['A', 'B', 'C', 'D']
+
+def get_arrows(prog_string):
+    prog_states = [
+        action[2]
+        for action in
+        prog_string.split()
+    ]
+
+    arrows = {
+        state: set()
+        for state in
+        STATES
+    }
+
+    for i, state in enumerate(prog_states):
+        arrows[STATES[i // 2]].add(state)
+
+    return arrows
+
+
+def dump_dot(arrows):
+    return 'digraph NAME {{\n{}\n}}'.format('\n'.join([
+        f'  {node} -> {target};'
+        for node, targets in arrows.items()
+        for target in targets
+    ]))
+
+
+if __name__ == '__main__':
+    print(
+        dump_dot(
+            get_arrows(
+                sys.argv[1])))
