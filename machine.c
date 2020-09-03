3de3e234a2de2c4b1cfe5a1cf5a12771f9ba804f
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-02 22:22:22 -0500

    working out exit

--- machine.c
+++ machine.c
@@ -6,20 +6,32 @@
 #define X_LIMIT 100000
 #define TAPE_LEN ((X_LIMIT * 2) + 10)
 
-#define CHECK_X do {x_count++; if (x_count > X_LIMIT) {goto H;}} while (0)
+#define CHECK_X(COUNT) do {                     \
+    x_count++;                                  \
+    if (x_count > X_LIMIT) {goto H;};           \
+    COUNT = x_count;                            \
+  } while (0)
 
 int POS = TAPE_LEN / 2;
 
 int TAPE[TAPE_LEN];
 
-#define ZERO_TAPE for (int i = 0; i < TAPE_LEN; i++) {TAPE[i] = 0;}
+#define ZERO_TAPE for (int i = 0; i < TAPE_LEN; i++) { TAPE[i] = 0; }
 
 #define L POS--
 #define R POS++
 
+#define ACTION(c, s, t) {                       \
+    TAPE[POS] = c;                              \
+    if (s - 76) { R; } else { L; };             \
+    goto *dispatch[t - 65];                     \
+  }
+
 #define INSTRUCTION(c0, s0, t0, c1, s1, t1)     \
-  if (TAPE[POS]) {TAPE[POS] = c1; s1; goto t1;} \
-  else {TAPE[POS] = c0; s0; goto t0;}
+  if (TAPE[POS])                                \
+    ACTION(c1, s1, t1)                          \
+    else                                        \
+      ACTION(c0, s0, t0)
 
 int x_count = 0;
 
@@ -44,33 +56,41 @@ char c0, c1, c2, c3, c4, c5, c6, c7,
   getc(stdin);
 
 int main (void) {
-  void* dispatch[] { &&A, &&B, &&C, &&D };
+  static void* dispatch[] = { &&A, &&B, &&C, &&D };
 
  INITIALIZE:
   ZERO_TAPE;
   LOAD_PROGRAM;
 
  A:
-  CHECK_X; a_count = x_count;
-
-  INSTRUCTION(1, R, B, 1, R, C);
+  CHECK_X(a_count);
+  INSTRUCTION(c0, c1, c2, c3, c4, c5);
 
  B:
-  CHECK_X; b_count = x_count;
-
-  INSTRUCTION(1, L, C, 1, R, D);
+  CHECK_X(b_count);
+  INSTRUCTION(c6, c7, c8, c9, c10, c11);
 
  C:
-  CHECK_X; c_count = x_count;
+  CHECK_X(c_count);
 
-  INSTRUCTION(1, R, A, 1, L, D);
+  INSTRUCTION(c12, c13, c14, c15, c16, c17);
 
  D:
-  CHECK_X; d_count = x_count;
+  CHECK_X(d_count);
 
-  INSTRUCTION(0, R, D, 0, L, B);
+  INSTRUCTION(c18, c19, c20, c21, c22, c23);
 
  H:
-  printf("%s | %d %d %d %d\n", PROG, a_count, b_count, c_count, d_count);
+  printf("%c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c | %d %d %d %d\n",
+         c0, c1, c2, c3, c4, c5, c6, c7,
+         c8, c9, c10, c11, c12, c13, c14, c15,
+         c16, c17, c18, c19, c20, c21, c22, c23,
+         a_count, b_count, c_count, d_count);
+
+  a_count = b_count = c_count = d_count = 0;
+
+  goto INITIALIZE;
+
+ EXIT:
   exit(0);
 }
