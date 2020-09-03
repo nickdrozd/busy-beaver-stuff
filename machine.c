f00c6c9c086d449111eb2da48e497db9b913e24e
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-02 23:27:31 -0500

    H -> PRINT

--- machine.c
+++ machine.c
@@ -8,7 +8,7 @@
 
 #define CHECK_X(COUNT) do {                     \
     XX++;                                       \
-    if (XX > X_LIMIT) {goto H;};                \
+    if (XX > X_LIMIT) {goto PRINT;};                \
     COUNT = XX;                                 \
   } while (0)
 
@@ -79,7 +79,7 @@ int main (void) {
   CHECK_X(DD);
   INSTRUCTION(c18, c19, c20, c21, c22, c23);
 
- H:
+ PRINT:
   printf("%c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c | %d %d %d %d\n",
          c0, c1, c2, c3, c4, c5, c6, c7,
          c8, c9, c10, c11, c12, c13, c14, c15,
