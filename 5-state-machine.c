470b9be655916cbe8e1b60ebd2beb3d0f07d1a7c
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-03 14:59:59 -0500

    Forgot some printing

--- 5-state-machine.c
+++ 5-state-machine.c
@@ -84,12 +84,14 @@ int main (void) {
   INSTRUCTION(c24, c25, c26, c27, c28, c29);
 
  H:
-  printf("%d | 1RB %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c | %d %d %d %d\n",
+  printf("%d | 1RB %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c %c%c%c | %d %d %d %d %d\n",
          PP,
-         c3, c4, c5, c6, c7,
-         c8, c9, c10, c11, c12, c13, c14, c15,
-         c16, c17, c18, c19, c20, c21, c22, c23,
-         AA, BB, CC, DD);
+         c3, c4, c5,
+         c6, c7, c8, c9, c10, c11,
+         c12, c13, c14, c15, c16, c17,
+         c18, c19, c20, c21, c22, c23,
+         c24, c25, c26, c27, c28, c29,
+         AA, BB, CC, DD, EE);
 
   goto INITIALIZE;
 
