a3fae4d9c17415127584fab87b88ac245ae3a06d
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-05 15:51:28 -0500

    Prohibit B0 -> 1RA

--- gen5.py
+++ gen5.py
@@ -89,6 +89,9 @@ def decorate(arrows):
 
             c3, c4, c5, c6, c7, c8, c9, c10 = colors
 
+            if  c3  1 == and s3 == R and n3 == A:
+                continue
+
             yield f'1RB 1{s2}C {c3}{s3}{n3} {c4}{s4}{n4} {c5}{s5}{n5} {c6}{s6}{n6} {c7}{s7}{n7} {c8}{s8}{n8} {c9}{s9}{n9} {c10}{s10}{n10}'
 
 
