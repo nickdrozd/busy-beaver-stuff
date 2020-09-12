e2a15e9fd22f6df73a63768ddb7f4ae70f3c07bf
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-12 11:02:38 -0500

    Record state counts in bb5

--- bb5.c
+++ bb5.c
@@ -3,8 +3,7 @@
 
 #define PROG "1RB 1LC 1RC 1RB 1RD 0LE 1LA 1LD 1RH 0LA"
 
-#define X_LIMIT 100000
-#define TAPE_LEN ((X_LIMIT * 2) + 10)
+#define TAPE_LEN ((100000 * 2) + 10)
 
 int POS = TAPE_LEN / 2;
 
@@ -15,40 +14,50 @@ int TAPE[TAPE_LEN];
 #define L POS--
 #define R POS++
 
-#define INSTRUCTION(c0, s0, t0, c1, s1, t1) \
-  if (TAPE[POS]) {TAPE[POS] = c1; s1; goto t1;} else {TAPE[POS] = c0; s0; goto t0;}
+#define INSTRUCTION(c0, s0, t0, c1, s1, t1)     \
+  if (TAPE[POS])                                \
+    {TAPE[POS] = c1; s1; goto t1;}              \
+  else                                          \
+    {TAPE[POS] = c0; s0; goto t0;}
 
-int x_count = 0;
+int XX = 0;
+int AA = 0;
+int BB = 0;
+int CC = 0;
+int DD = 0;
+int EE = 0;
+
+#define INCREMENT(COUNT) XX++; COUNT++;
 
 int main (void) {
   ZERO_TAPE;
 
  A:
-  x_count++;
+  INCREMENT(AA);
 
   INSTRUCTION(1, R, B, 1, L, C);
 
  B:
-  x_count++;
+  INCREMENT(BB);
 
   INSTRUCTION(1, R, C, 1, R, B);
 
  C:
-  x_count++;
+  INCREMENT(CC);
 
   INSTRUCTION(1, R, D, 0, L, E);
 
  D:
-  x_count++;
+  INCREMENT(DD);
 
   INSTRUCTION(1, L, A, 1, L, D);
 
  E:
-  x_count++;
+  INCREMENT(EE);
 
   INSTRUCTION(1, R, H, 0, L, A);
 
  H:
-  printf("%s | %d\n", PROG, x_count);
+  printf("%s | %d | %d %d %d %d %d\n", PROG, XX, AA, BB, CC, DD, EE);
   exit(0);
 }
