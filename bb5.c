365b5fd69fed2e66739a13db3035efdc0f199c2a
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-14 20:15:23 -0500

    Minor bb5 changes

--- bb5.c
+++ bb5.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define PROG "1RB 1LC 1RC 1RB 1RD 0LE 1LA 1LD 1RH 0LA"
 
 #define TAPE_LEN ((100000 * 2) + 10)
 
 int POS = TAPE_LEN / 2;
-
 int TAPE[TAPE_LEN];
 
 #define ZERO_TAPE for (int i = 0; i < TAPE_LEN; i++) {TAPE[i] = 0;}
@@ -20,44 +18,35 @@ int TAPE[TAPE_LEN];
   else                                          \
     {TAPE[POS] = c0; s0; goto t0;}
 
-int XX = 0;
-int AA = 0;
-int BB = 0;
-int CC = 0;
-int DD = 0;
-int EE = 0;
+int XX, AA, BB, CC, DD, EE;
 
+#define ZERO_COUNTS XX = AA = BB = CC = DD = EE = 0;
 #define INCREMENT(COUNT) XX++; COUNT++;
 
 int main (void) {
   ZERO_TAPE;
+  ZERO_COUNTS;
 
  A:
   INCREMENT(AA);
-
   INSTRUCTION(1, R, B, 1, L, C);
 
  B:
   INCREMENT(BB);
-
   INSTRUCTION(1, R, C, 1, R, B);
 
  C:
   INCREMENT(CC);
-
   INSTRUCTION(1, R, D, 0, L, E);
 
  D:
   INCREMENT(DD);
-
   INSTRUCTION(1, L, A, 1, L, D);
 
  E:
   INCREMENT(EE);
-
   INSTRUCTION(1, R, H, 0, L, A);
 
  H:
   printf("%s | %d | %d %d %d %d %d\n", PROG, XX, AA, BB, CC, DD, EE);
-  exit(0);
 }
