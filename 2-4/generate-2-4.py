a8ed2434da5d261faf680a8b6b1ec732b8a6a90c
Nick Drozd <nicholasdrozd@gmail.com>
2020-09-30 13:05:41 -0500

    2-4: more stringent program generation requirements

--- 2-4/generate-2-4.py
+++ 2-4/generate-2-4.py
@@ -1,7 +1,7 @@
 import re
 from itertools import filterfalse, product
 
-COLORS = 0, 1, 2, 3
+COLORS = 1, 2, 3
 SHIFTS = L, R = 'L', 'R'
 STATES = A, B = 'A', 'B'
 
@@ -18,8 +18,7 @@ def is_normal(colors):
 
 def yield_progs():
     for colors in product(COLORS, repeat=7):
-        if (colors.count(0) > 1
-            or 1 not in colors
+        if (1 not in colors
             or 2 not in colors
             or 3 not in colors):
             continue
@@ -28,6 +27,11 @@ def yield_progs():
             continue
 
         c2, c3, c4, c5, c6, c7, c8 = colors
+
+        if (c2 == 1
+            or c2 == c3 == c4
+            or c5 == c6 == c7 == c8):
+            continue
 
         for states in product(STATES, repeat=7):
             t2, t3, t4, t5, t6, t7, t8 = states
@@ -38,18 +42,17 @@ def yield_progs():
                 continue
 
             for shifts in product(SHIFTS, repeat=7):
-                if not (2 < shifts.count(L) < 6):
-                    continue
-
                 s2, s3, s4, s5, s6, s7, s8 = shifts
+
+                if s2 == s3 == s4 or s5 == s6 == s7 == s8:
+                    continue
 
                 yield f'1RB {c2}{s2}{t2} {c3}{s3}{t3} {c4}{s4}{t4} {c5}{s5}{t5} {c6}{s6}{t6} {c7}{s7}{t7} {c8}{s8}{t8}'
 
 
 regexps = (
-    '^1RB 0.A ... ... 1LA',
     '^1RB ... ... ... 1R.',
-    '^1RB 0LA ... ... 1LB 1.A',
+    '^1RB ... ... ... 1LB 1.B',
 )
 
 compiled_regexps = [re.compile(regexp) for regexp in regexps]
