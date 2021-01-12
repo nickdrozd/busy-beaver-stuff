b86363a953331b0699c0ce1625918094ea2e63e0
Nick Drozd <nicholasdrozd@gmail.com>
2021-01-11 21:53:03 -0600

    Separate fast and slow tests, redo test comments

--- test/test_turing.py
+++ test/test_turing.py
@@ -4,37 +4,23 @@ from unittest import TestCase
 
 from turing import run_bb
 
-HALTING = {
-    # BB_2_2
+HALTING_FAST = {
+    # 2/2 BB
     "1RB 1LB 1LA 1RH": (4, 6),
 
-    # BB_3_2 shift
-    "1RB 1RH 1LB 0RC 1LC 1LA": (5, 21),
-    # BB_3_2 sigma
-    "1RB 1LC 1RC 1RH 1LA 0LB": (6, 11),
+    # 3/2 BB
+    "1RB 1RH 1LB 0RC 1LC 1LA": (5, 21),  # shift
+    "1RB 1LC 1RC 1RH 1LA 0LB": (6, 11),  # sigma
 
-    # BB_2_3
+    # 2/3 BB
     "1RB 2LB 1RH 2LA 2RB 1LB": (9, 38),
 
-    # BB_4_2 shift
-    "1RB 1LB 1LA 0LC 1RH 1LD 1RD 0RA": (13, 107),
-    # BB_4_2 sigma
-    "1RB 0RC 1LA 1RA 1RH 1RD 1LD 0LB": (13, 96),
-
-    # SIAB_3_3
-    "1RB 2LB 1LC 1LA 2RB 1RB 1RH 2LA 0LC": (31, 2315619),
+    # 4/2 BB
+    "1RB 1LB 1LA 0LC 1RH 1LD 1RD 0RA": (13, 107),  # shift
+    "1RB 0RC 1LA 1RA 1RH 1RD 1LD 0LB": (13,  96),  # sigma
 
     # RU_2_4
     "1RB 3LA 1LA 1RA 2LA 1RH 3RA 3RB": (90, 7195),
-
-    # BB_2_4
-    "1RB 2LA 1RA 1RA 1LB 1LA 3RB 1RH": (2050, 3932964),
-
-    # COPY_3_3_2_4
-    "1RB 1LC 1RH 1LA 1LC 2RB 1RB 2LC 1RC": (2050, 3932964),
-
-    # BB_5_2
-    "1RB 1LC 1RC 1RB 1RD 0LE 1LA 1LD 1RH 0LA": (4098, 47176870),
 
     # Milton Green (1964)
     "1RB 1LA 0LH 1RB": (1, 2),
@@ -68,25 +54,38 @@ HALTING = {
 }
 
 
+HALTING_SLOW = {
+    # 3/3 Surprise-in-a-box
+    "1RB 2LB 1LC 1LA 2RB 1RB 1RH 2LA 0LC": (31, 2315619),
+
+    # 2/4 BB
+    "1RB 2LA 1RA 1RA 1LB 1LA 3RB 1RH": (2050, 3932964),
+
+    # 3/3 copy of 2/4 BB
+    "1RB 1LC 1RH 1LA 1LC 2RB 1RB 2LC 1RC": (2050, 3932964),
+
+    # 5/2 BB
+    "1RB 1LC 1RC 1RB 1RD 0LE 1LA 1LD 1RH 0LA": (4098, 47176870),
+}
+
+
 QUASIHALTING = {
-    # 2_2 (not better than BB)
+    # 2/2 (not better than BB)
     "1RB 1LB 1LB 1LA": (3, 6, 1),
     "1RB 1LB 0LB 1LA": (2, 6, 1),
     "1RB 0LB 1LB 1LA": (2, 6, 1),
     "1RB 0LB 0LB 1LA": (1, 6, 1),
 
-    # BBB_2_3_sigma
-    "1RB 2LB 1RA 2LB 2LA 0RA": (10, 43, 1),
-    # BBB_2_3_shift
-    "1RB 2LB 1LA 2LB 2RA 0RA":  (8, 59, 1),
+    # 2/3
+    "1RB 2LB 1RA 2LB 2LA 0RA": (10, 43, 1),  # BBB sigma
+    "1RB 2LB 1LA 2LB 2RA 0RA":  (8, 59, 1),  # BBB shift
     "1RB 0LB 1RA 1LB 2LA 2RA":  (3, 45, 1),
     "1RB 2RA 2LB 2LB 2LA 0LA":  (5, 40, 1),
     "1RB 1LA 2RA 2LA 2LB 2RB":  (8, 17, 2),
 
-    # BBB_3_2 shift
-    "1RB 0LB 1LA 0RC 1LC 1LA": (6, 55, 1),
-    # BBB_3_2 sigma
-    "1RB 1RC 1LC 1RA 1RA 1LA": (6,  9, 2),
+    # 3/2
+    "1RB 0LB 1LA 0RC 1LC 1LA": (6, 55, 1),  # BBB shift
+    "1RB 1RC 1LC 1RA 1RA 1LA": (6,  9, 2),  # BBB sigma
     "1RB 0LB 1RC 0RC 1LC 1LA": (6, 54, 1),
     "1RB 0LC 1LB 0RC 1LC 1LA": (5, 52, 1),
     "1RB 0LC 0LC 0RC 1LC 1LA": (5, 51, 1),
@@ -94,10 +93,9 @@ QUASIHALTING = {
     "1RB 0LC 0RC 0RC 1LC 1LA": (5, 48, 1),
     "1RB 1RC 1LC 0LB 1RA 1LA": (5, 22, 2),
 
-    # BBB_4_2_shift
-    "1RB 0LC 1LD 0LA 1RC 1RD 1LA 0LD": (0, 66349, 1),
-    # BBB_4_2_sigma
-    "1RB 1RC 1LC 1RD 1RA 1LD 0RD 0LB": (69, 2819, 1),
+    # 4/2
+    "1RB 0LC 1LD 0LA 1RC 1RD 1LA 0LD": (0, 66349, 1),  # BBB shift
+    "1RB 1RC 1LC 1RD 1RA 1LD 0RD 0LB": (69, 2819, 1),  # BBB sigma
     "1RB 1RA 0RC 0RB 0RD 1RA 1LD 1LB": ( 0, 2568, 1),
     "1RB 1RA 0RC 1LA 1LC 1LD 0RB 0RD": ( 0, 2512, 1),
     "1RB 1RC 1RD 0LC 1LD 0LD 1LB 0RA": (56, 2332, 3),
@@ -106,7 +104,7 @@ QUASIHALTING = {
     "1RB 1LC 1LC 0RD 1LA 0LB 1LD 0RA": (39, 1164, 1),
     "1RB 1LB 1RC 0LD 0RD 0RA 1LD 0LA": (20, 1153, 1),
 
-    # 5_2
+    # 5/2
     "1RB 1LC 1LC 1RA 1LB 0LD 1LA 0RE 1RD 1RE": (504, 221032, 2),
 }
 
@@ -117,12 +115,12 @@ RECURRENCE = {
     "1RB 1RH 1LB 0LC 1LA 1RA": (12,  7),  # left barrier
     "1RB 1RH 1LC 1RA 1LA 0LC": (12,  8),  # right barrier
 
-    # 2-2
+    # 2/2
     "1RB 0LB 1LA 0RB": (9, 3),
     "1RB 1LA 0LA 1RA": (7, 5),
     "1RB 1LB 1LA 0RB": (7, 3),
 
-    # 3-2
+    # 3/2
     "1RB 1LB 0RC 0LA 1LC 0LA": (101, 24),
     "1RB 1LA 1LC 1RC 1LA 0RB": ( 69, 16),
     "1RB 1LB 1RC 0LA 1LA 1RC": ( 65, 16),
@@ -134,7 +132,7 @@ RECURRENCE = {
     "1RB 0LA 0RC 1LA 1LC 0RB": (  0, 92),
     "1RB 0LA 0RC 0RC 1LC 1LA": (  0, 48),
 
-    # 2-3
+    # 2/3
     "1RB 0LA 0RH 1LB 2LA 0RB": (165, 54),
     "1RB 0LA 0LH 1LB 2LA 0RB": (165, 54),
     "1RB 0LA 1RH 1LB 2LA 0RB": (165, 54),
@@ -176,7 +174,7 @@ RECURRENCE = {
     "1RB 2LB 0RA 1LA 2RB 2RA": (  0, 60),
     "1RB 2LA 1LB 0LA 0RB 1RA": (  0, 47),
 
-    # 4-2
+    # 4/2
     "1RB 1RC 1LC 0RB 1LD 0RA 1RA 0LB": (1727, 622),
     "1RB 0LC 1RD 0RD 1LA 0RC 1LB 1RC": (1527, 522),
     "1RB 0LC 1RC 1RD 1LD 0RC 1LA 0RB": (1301, 622),
@@ -221,14 +219,20 @@ class TuringTest(TestCase):
 
         self.machine = run_bb(prog, **opts)
 
-    def test_halting(self):
-        for prog, (marks, steps) in HALTING.items():
+    def _test_halting(self, prog_data):
+        for prog, (marks, steps) in prog_data.items():
             self.run_bb(prog)
 
             self.assert_marks(marks)
             self.assert_steps(steps)
 
             self.assert_final('HALTED')
+
+    def test_halting_fast(self):
+        self._test_halting(HALTING_FAST)
+
+    def test_halting_slow(self):
+        self._test_halting(HALTING_SLOW)
 
     def test_quasihalting(self):
         for prog, (marks, steps, period) in QUASIHALTING.items():
