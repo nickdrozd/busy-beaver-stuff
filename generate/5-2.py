2ad95e9e057dc2c97c500567e4c944b8368b2cc0
Nick Drozd <nicholasdrozd@gmail.com>
2021-02-26 15:25:27 -0600

    Use Graph for 5-2 generation

--- generate/5-2.py
+++ generate/5-2.py
@@ -1,6 +1,9 @@
 import re
 from itertools import permutations, product, filterfalse
 
+from graph import Graph
+
+
 STATES = A, B, C, D, E = 'A', 'B', 'C', 'D', 'E'
 
 SHIFTS = R, L = 'R', 'L'
@@ -8,76 +11,30 @@ SHIFTS = R, L = 'R', 'L'
 COLORS = 0, 1
 
 
-def graph_to_string(graph, sep=''):
-    return sep.join(
-        dst
-        for state in STATES
-        for conn in graph[state]
-        for dst in conn
-    )
-
-
 def yield_graphs():
-    for b in permutations(STATES, r=2):
-        b0, _b1 = b
+    a = B, C
+    for b0, b1 in permutations(STATES, r=2):
         if b0 == A:
             continue
         for c in permutations(STATES, r=2):
             for d in permutations(STATES, r=2):
                 for e in permutations(STATES, r=2):
-                    yield {A: (B, C), B: b, C: c, D: d, E: e,}
+                    graph = Graph((*a, b0, b1, *c, *d, *e))
+                    if graph.is_normal and graph.is_strongly_connected:
+                        if only_d_self_connected(graph):
+                            yield graph
 
 
-def is_normal(graph):
-    graph_string = graph_to_string(graph)
-
-    if any(state not in graph_string for state in STATES):
+def only_d_self_connected(graph):
+    if D not in graph.arrows[D]:
         return False
 
-    c = graph_string.find(C)
-    d = graph_string.find(D)
-    e = graph_string.find(E)
-
-    return c < d < e
-
-
-def is_strongly_connected(arrows):
-    for state in STATES:
-        if all(state not in arrows[dst]
-               for dst in set(STATES).difference(state)):
-            return False
-
-    for state in STATES:
-        reachable_from_x = set(arrows[state]).difference(state)
-
-        for _ in range(3):
-            reachable = {
-                node
-                for connection in reachable_from_x
-                for node in arrows[connection]
-            }
-
-            reachable_from_x.update(reachable)
-
-        if not reachable_from_x.issuperset(STATES):
-            return False
-
-    return True
-
-
-def only_d_self_connected(arrows):
-    if D not in arrows[D]:
-        return False
-
-    return all(state not in arrows[state]
+    return all(state not in graph.arrows[state]
                for state in set(STATES).difference(D))
 
 
-def decorate(arrows):
-    n3, n4 = arrows[B]
-    n5, n6 = arrows[C]
-    n7, n8 = arrows[D]
-    n9, n10 = arrows[E]
+def decorate(graph):
+    _, _, n3, n4, n5, n6, n7, n8, n9, n10 = graph.flatten().split()
 
     for shifts in product(SHIFTS, repeat=9):
         L_count = shifts.count(L)
@@ -115,15 +72,7 @@ def compress(prog_string):
 
 
 def main():
-    graphs = filter(
-        only_d_self_connected,
-        filter(
-            is_strongly_connected,
-            filter(
-                is_normal,
-                yield_graphs())))
-
-    for graph in graphs:
+    for graph in yield_graphs():
         for prog in filterfalse(is_obviously_stupid, decorate(graph)):
             print(compress(prog))
 
