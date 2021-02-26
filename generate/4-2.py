ffcb1825166f8eb9493d8f86110f8c0cfb586ab5
Nick Drozd <nicholasdrozd@gmail.com>
2021-02-26 14:49:27 -0600

    Use Graph for 4-2 generation

--- generate/4-2.py
+++ generate/4-2.py
@@ -1,6 +1,9 @@
 import re
 from itertools import permutations, product, filterfalse
 
+from graph import Graph
+
+
 STATES = A, B, C, D = 'A', 'B', 'C', 'D'
 
 SHIFTS = R, L = 'R', 'L'
@@ -8,67 +11,20 @@ SHIFTS = R, L = 'R', 'L'
 COLORS = 0, 1
 
 
-def graph_to_string(graph, sep=''):
-    return sep.join(
-        dst
-        for state in STATES
-        for conn in graph[state]
-        for dst in conn
-    )
-
-
 def yield_graphs():
-    for a1 in (A, C, D):
-        for b in permutations(STATES, r=2):
-            b0, _ = b
+    for a1 in A, C, D:
+        for b0, b1 in permutations(STATES, r=2):
             if b0 == A:
                 continue
             for c in permutations(STATES, r=2):
                 for d in permutations(STATES, r=2):
-                    yield {A: (B, a1), B: b, C: c, D: d}
+                    graph = Graph((B, a1, b0, b1, *c, *d))
+                    if graph.is_normal and graph.is_strongly_connected:
+                        yield graph
 
 
-def is_normal(graph):
-    graph_string = graph_to_string(graph)
-
-    if any(state not in graph_string for state in STATES):
-        return False
-
-    c = graph_string.find(C)
-    d = graph_string.find(D)
-
-    return c < d
-
-
-def is_strongly_connected(arrows):
-    for state in STATES:
-        if all(state not in arrows[dst]
-               for dst in set(STATES).difference(state)):
-            return False
-
-    for state in STATES:
-        reachable_from_x = set(arrows[state]).difference(state)
-
-        for _ in range(3):
-            reachable = {
-                node
-                for connection in reachable_from_x
-                for node in arrows[connection]
-            }
-
-            reachable_from_x.update(reachable)
-
-        if not reachable_from_x.issuperset(STATES):
-            return False
-
-    return True
-
-
-def decorate(arrows):
-    n1, n2 = arrows[A]
-    n3, n4 = arrows[B]
-    n5, n6 = arrows[C]
-    n7, n8 = arrows[D]
+def decorate(graph):
+    n1, n2, n3, n4, n5, n6, n7, n8 = graph.flatten().split()
 
     for shifts in product(SHIFTS, repeat=7):
         L_count = shifts.count(L)
@@ -109,20 +65,10 @@ def is_obviously_stupid(prog_string):
     return False
 
 
-def compress(prog_string):
-    return prog_string[4:].replace(' ', '')
-
-
 def main():
-    graphs = filter(
-        is_strongly_connected,
-        filter(
-            is_normal,
-            yield_graphs()))
-
-    for graph in graphs:
+    for graph in yield_graphs():
         for prog in filterfalse(is_obviously_stupid, decorate(graph)):
-            print((prog))
+            print(prog)
 
 
 if __name__ == '__main__':
