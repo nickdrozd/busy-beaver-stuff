19462a48424ff53a758eedbf11dd834ca5fd2339
Nick Drozd <nicholasdrozd@gmail.com>
2021-06-19 22:05:41 -0500

    Move History to recurrence

--- tm/recurrence.py
+++ tm/recurrence.py
@@ -1,3 +1,36 @@
+from collections import defaultdict
+
+class History:
+    def __init__(self, tapes=None):
+        self.tapes = [] if tapes is None else tapes
+        self.beeps = []
+        self.states = []
+        self.actions = defaultdict(lambda: [])
+        self.positions = []
+
+    def calculate_beeps(self, through=None):
+        states = (
+            self.states
+            if through is None else
+            self.states[:through]
+        )
+
+        steps = len(states)
+        rev   = list(reversed(states))
+
+        return {
+            state: steps - 1 - rev.index(state)
+            for state in set(states)
+        }
+
+    def check_for_recurrence(self, step, action):
+        for pstep in self.actions[action]:
+            if verify_lin_recurrence(pstep, step - pstep, self):
+                return pstep, step - pstep
+
+        return None
+
+
 def verify_lin_recurrence(steps, period, history):
     tapes     = history.tapes
     states    = history.states
