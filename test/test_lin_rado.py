06bb88e322b6503ce31447243f0d081af4720dbb
Nick Drozd <nicholasdrozd@gmail.com>
2021-10-01 16:32:09 -0500

    Consolidate generation tests

--- test/test_lin_rado.py
+++ test/test_lin_rado.py
@@ -6,8 +6,39 @@ from multiprocessing import Queue
 from unittest import TestCase, skip
 
 from tm import run_bb
-from generate.naive import yield_programs
 from generate.tree import run_tree_gen
+from generate.naive import yield_programs
+
+
+class TestNaive(TestCase):
+    def yield_programs(self, states, colors, halt):
+        self.progs = tuple(
+            yield_programs(
+                states,
+                colors,
+                halt))
+
+    def assert_count(self, count):
+        self.assertEqual(
+            len(self.progs),
+            count)
+
+    def test_naive(self):
+        expected = {
+            (2, 2, 1): 64,
+            (2, 2, 0): 256,
+
+            (2, 3, 1): 41472,
+            (2, 3, 0): 124416,
+
+            (3, 2, 1): 57024,
+            (3, 2, 0): 186624,
+        }
+
+        for (states, colors, halt), count in expected.items():
+            print(states, colors, halt)
+            self.yield_programs(states, colors, halt)
+            self.assert_count(count)
 
 
 class TestLinRado(TestCase):
