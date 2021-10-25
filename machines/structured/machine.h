b301e85d49a130ed9fa4605028c2dc3b9ebade25
Nick Drozd <nicholasdrozd@gmail.com>
2021-10-24 20:22:46 -0500

    Redefine scan macro

--- machines/structured/machine.h
+++ machines/structured/machine.h
@@ -27,14 +27,15 @@ long STEPS = 0;
 long MARKS = 0;
 #define HALT_IF_BLANK if (!MARKS && STEPS > 0) goto H;
 
-#define SCAN(COLOR) (TAPE[POS] == COLOR)
+#define SCAN TAPE[POS]
+
 #define WRITE(COLOR) do {                       \
-    if (COLOR && !TAPE[POS]) MARKS++;           \
-    if (!COLOR && TAPE[POS]) MARKS--;           \
+    if (COLOR && !SCAN) MARKS++;                \
+    if (!COLOR && SCAN) MARKS--;                \
     TAPE[POS] = COLOR;                          \
   } while (0)
 
-#define BLANK !TAPE[POS]
+#define BLANK !SCAN
 #define PRINT WRITE(1)
 #define ERASE WRITE(0)
 
