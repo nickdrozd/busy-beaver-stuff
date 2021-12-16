520459782e87dc7b59fed31be36de8eaf6fcde55
Nick Drozd <nicholasdrozd@gmail.com>
2021-12-16 18:40:57 -0500

    Roll up B

--- machines/structured/2333909.c
+++ machines/structured/2333909.c
@@ -10,54 +10,79 @@ int main(void)
   WRITE(1);
   RIGHT;
 
- B:
-  switch (SCAN) {
-    case 0:
-      // B0
-      CHECK_RECUR(L);
-      WRITE(1);
-      LEFT;
-      goto B;
-    case 1:
-      // B1
-      WRITE(3);
-      RIGHT;
-      goto A;
-    case 2:
-      // B2
-      WRITE(3);
-      LEFT;
-      goto A;
-    case 3:
-      // B3
-      WRITE(2);
-      RIGHT;
-      goto B;
-  }
+  while (1) {
+    switch (SCAN) {
+      case 0:
+        // B0
+        CHECK_RECUR(L);
+        WRITE(1);
+        LEFT;
+        continue;
 
- A:
-  while (SCAN == 3) {
-    // A3
-    WRITE(1);
-    LEFT;
-  }
+      case 1:
+        // B1
+        WRITE(3);
+        RIGHT;
 
-  switch (SCAN) {
-    case 0:
-      // A0
-      WRITE(1);
-      RIGHT;
-      goto B;
-    case 1:
-      // A1
-      WRITE(2);
-      RIGHT;
-      goto B;
-    case 2:
-      // A2
-      WRITE(1);
-      LEFT;
-      goto B;
+        while (SCAN == 3) {
+          // A3
+          WRITE(1);
+          LEFT;
+        }
+
+        switch (SCAN) {
+          case 0:
+            // A0
+            WRITE(1);
+            RIGHT;
+            continue;
+          case 1:
+            // A1
+            WRITE(2);
+            RIGHT;
+            continue;
+          case 2:
+            // A2
+            WRITE(1);
+            LEFT;
+            continue;
+        }
+
+      case 2:
+        // B2
+        WRITE(3);
+        LEFT;
+
+        while (SCAN == 3) {
+          // A3
+          WRITE(1);
+          LEFT;
+        }
+
+        switch (SCAN) {
+          case 0:
+            // A0
+            WRITE(1);
+            RIGHT;
+            continue;
+          case 1:
+            // A1
+            WRITE(2);
+            RIGHT;
+            continue;
+          case 2:
+            // A2
+            WRITE(1);
+            LEFT;
+            continue;
+        }
+
+      case 3:
+        // B3
+        WRITE(2);
+        RIGHT;
+        continue;
+    }
   }
 
  H:
