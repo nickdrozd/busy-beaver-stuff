21a0d9d91150903cb726c2a3fcde0e17fce6d306
Nick Drozd <nicholasdrozd@gmail.com>
2021-12-16 18:20:10 -0500

    Add 2333909

---
+++ machines/structured/2333909.c
@@ -0,0 +1,60 @@
+#define PROGRAM "1RB 2RB 1LB 1LA  1LB 3RA 3LA 2RB"
+
+#define TAPELEN 4065
+
+#include "machine.h"
+
+int main(void)
+{
+ A:
+  switch (SCAN) {
+    case 0:
+      // A0
+      WRITE(1);
+      RIGHT;
+      goto B;
+    case 1:
+      // A1
+      WRITE(2);
+      RIGHT;
+      goto B;
+    case 2:
+      // A2
+      WRITE(1);
+      LEFT;
+      goto B;
+    case 3:
+      // A3
+      WRITE(1);
+      LEFT;
+      goto A;
+  }
+
+ B:
+  switch (SCAN) {
+    case 0:
+      // B0
+      CHECK_RECUR(L);
+      WRITE(1);
+      LEFT;
+      goto B;
+    case 1:
+      // B1
+      WRITE(3);
+      RIGHT;
+      goto A;
+    case 2:
+      // B2
+      WRITE(3);
+      LEFT;
+      goto A;
+    case 3:
+      // B3
+      WRITE(2);
+      RIGHT;
+      goto B;
+  }
+
+ H:
+  CHECK_STEPS;
+}
