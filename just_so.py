81151943b3d9033a91b07accd472badc8d13e02c
Nick Drozd <nicholasdrozd@gmail.com>
2022-03-03 08:25:47 -0600

    Add just_so

---
+++ just_so.py
@@ -0,0 +1,11 @@
+import sys
+
+from generate.program import Program
+
+if __name__ == '__main__':
+    for prog in map(Program, sys.stdin):
+        if prog.can_spin_out:
+            try:
+                print(prog)
+            except BrokenPipeError:
+                sys.exit()
