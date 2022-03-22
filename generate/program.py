86572cd4411841b9e5a443fb88cf3aed473d7855
Nick Drozd <nicholasdrozd@gmail.com>
2022-03-22 18:22:56 -0500

    Reduce attempts

--- generate/program.py
+++ generate/program.py
@@ -276,7 +276,7 @@ class Program:
             self,
             final_prop: str,
             slots: Iterator[str],
-            max_attempts: int = 10,
+            max_attempts: int = 6,
             blank: bool = False,
             spinout: bool = False,
     ):
