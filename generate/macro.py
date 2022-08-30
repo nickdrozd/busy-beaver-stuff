4ef4ea81a231c61736de34bc43989c04cc889489
Nick Drozd <nicholasdrozd@gmail.com>
2022-08-30 01:38:11 -0400

    Separate macro run simulator

--- generate/macro.py
+++ generate/macro.py
@@ -22,57 +22,70 @@ class MacroConverter:
                     cells))
         ).normalize()
 
-    def macro_comp(self, cells: int,) -> CompProg:
+    def macro_comp(self, cells: int) -> CompProg:
         return tuple(
             tuple(
-                self.run(st_sh, list(tape))
+                run_macro_simulator(
+                    st_sh,
+                    list(tape),
+                    self.prog,
+                    self.states,
+                    self.colors,
+                )
                 for tape in product(range(self.colors), repeat = cells)
             )
             for st_sh in range(2 * self.states)
         )
 
-    def run(self, st_sh: int, tape: Tape) -> Instr:
-        state, edge = divmod(st_sh, 2)
 
-        pos = 0 if edge == 0 else len(tape) - 1
+def run_macro_simulator(
+        st_sh: int,
+        tape: Tape,
+        prog,
+        state_count: int,
+        color_count: int,
+) -> Instr:
+    state, edge = divmod(st_sh, 2)
 
-        max_config = self.states * len(tape) * self.colors ** len(tape)
+    pos = 0 if edge == 0 else len(tape) - 1
 
-        for _ in range(max_config):
-            scan = tape[pos]
+    max_config = state_count * len(tape) * color_count ** len(tape)
 
-            assert (instr := self.prog[state][scan]) is not None
+    for _ in range(max_config):
+        scan = tape[pos]
 
-            color, shift, next_state = instr
+        assert (instr := prog[state][scan]) is not None
 
-            tape[pos] = color
+        color, shift, next_state = instr
 
-            pos += 1 if shift else -1
+        tape[pos] = color
 
-            if (state := next_state) == 30:
-                return (
-                    _tape_to_color(tape, self.colors),
-                    1,
-                    state,
-                )
+        pos += 1 if shift else -1
 
-            if 0 <= pos < len(tape):
-                continue
-
-            next_edge = 0 if pos < 0 else 1
-            edge_diff = 1 if pos < 0 else 0
-            out_state = (2 * state) + edge_diff
-
+        if (state := next_state) == 30:
             return (
-                _tape_to_color(tape, self.colors),
-                next_edge,
-                out_state,
+                tape_to_color(tape, color_count),
+                1,
+                state,
             )
 
-        return 0, 0, 0
+        if 0 <= pos < len(tape):
+            continue
+
+        next_edge = 0 if pos < 0 else 1
+        edge_diff = 1 if pos < 0 else 0
+        out_state = (2 * state) + edge_diff
+
+        return (
+            tape_to_color(tape, color_count),
+            next_edge,
+            out_state,
+        )
+
+    return 0, 0, 0
 
 
-def _tape_to_color(tape: Tape, colors: int) -> int:
+def tape_to_color(tape: Tape, colors: int) -> int:
     return int(
         ''.join(map(str, tape)),
         colors)
