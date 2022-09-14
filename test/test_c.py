7e6dd17dba593b082ef31cae59d41aa6bc9a772d
Nick Drozd <nicholasdrozd@gmail.com>
2022-09-14 00:42:20 -0400

    Add another C gen test case

--- test/test_c.py
+++ test/test_c.py
@@ -2,12 +2,20 @@ from unittest import TestCase
 
 from generate.c import make_c
 
+
+TEST_FILES = {
+    "1RB 1RC  1LC 1RD  1RA 1LD  0RD 0LB": 2819,
+
+    "1RB 2LA 1RA 1RA  1LB 1LA 3RB 1R_": 3932964,
+}
+
+
 class TestC(TestCase):
     def test_c(self):
-        with open('test/data/2819.c.test') as test:
-            expected = test.read()
-            actual = make_c("1RB 1RC  1LC 1RD  1RA 1LD  0RD 0LB")
+        for prog, name in TEST_FILES.items():
+            print(prog)
 
-            self.assertEqual(
-                expected,
-                actual)
+            with open(f'test/data/{name}.c.test') as test:
+                self.assertEqual(
+                    test.read(),
+                    make_c(prog))
