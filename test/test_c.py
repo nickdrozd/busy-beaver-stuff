fbef6e7769ee8a002ba986ae4fa920421dae1987
Nick Drozd <nicholasdrozd@gmail.com>
2022-09-14 13:46:31 -0400

    Add more C gen test cases

--- test/test_c.py
+++ test/test_c.py
@@ -5,8 +5,9 @@ from generate.c import make_c
 
 TEST_FILES = {
     "1RB 1RC  1LC 1RD  1RA 1LD  0RD 0LB": 2819,
-
     "1RB 2LA 1RA 1RA  1LB 1LA 3RB 1R_": 3932964,
+    "1RB 1LC  1RD 1RB  0RD 0RC  1LD 1LA": 32779477,
+    "1RB 1LC  1RC 1RB  1RD 0LE  1LA 1LD  1R_ 0LA": 47176870,
 }
 
 
