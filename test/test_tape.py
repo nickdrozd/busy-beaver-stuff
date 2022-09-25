71d7315bdc61ecbe3ce5e35be14a60d67757a4a6
Nick Drozd <nicholasdrozd@gmail.com>
2022-09-25 16:18:01 -0500

    Add slice test

--- test/test_tape.py
+++ test/test_tape.py
@@ -46,3 +46,64 @@ class TestTape(TestCase):
         self.assertEqual(
             copy_2.signature,
             '1|0|1[2]1')
+
+    def test_slice(self):
+        self.run_bb("1RB 2LB 1LA  2LB 2RA 0RA")
+
+        self.assertEqual(
+            str(self.tape),
+            "[0] 2^1 1^7")
+
+        self.assertEqual(
+            self.tape.head,
+            -3)
+
+        ptr = self.tape.to_ptr()
+
+        self.assertEqual(
+            ptr.tape,
+            [0, 2, 1, 1, 1, 1, 1, 1, 1])
+
+        self.assertEqual(
+            (ptr.l_end, ptr.init, ptr.r_end),
+            (-3, 3, 6))
+
+        self.assertEqual(
+            ptr[-3:0],
+            [0, 2, 1])
+
+        self.assertEqual(
+            (ptr.l_end, ptr.init, ptr.r_end),
+            (-3, 3, 6))
+
+        self.assertEqual(
+            ptr[-3:3],
+            [0, 2, 1, 1, 1, 1])
+
+        self.assertEqual(
+            (ptr.l_end, ptr.init, ptr.r_end),
+            (-3, 3, 6))
+
+        self.assertEqual(
+            ptr[-3:4],
+            [0, 2, 1, 1, 1, 1, 1])
+
+        self.assertEqual(
+            (ptr.l_end, ptr.init, ptr.r_end),
+            (-3, 3, 7))
+
+        self.assertEqual(
+            ptr[-3:6],
+            [0, 2, 1, 1, 1, 1, 1, 1, 1])
+
+        self.assertEqual(
+            (ptr.l_end, ptr.init, ptr.r_end),
+            (-3, 3, 9))
+
+        self.assertEqual(
+            ptr[-5:10],
+            [0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0])
+
+        self.assertEqual(
+            (ptr.l_end, ptr.init, ptr.r_end),
+            (-5, 5, 13))
