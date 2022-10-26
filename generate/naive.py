0efcb79694c5014c87033cd0b9074a4f104cc743
Nick Drozd <nicholasdrozd@gmail.com>
2022-10-26 19:00:12 -0400

    Rework naive generate

--- generate/naive.py
+++ generate/naive.py
@@ -1,99 +1,33 @@
 import re
 from itertools import product
-from collections.abc import Callable, Iterator, Sequence
+from collections.abc import Iterator, Sequence
 
 SHIFTS = 'R', 'L'
 HALT   = '_'
 
 Rejects = Sequence[str | re.Pattern[str]]
 
-def yield_all_programs(
-        state_count: int,
-        color_count: int,
+def yield_actions(
+        states: int,
+        colors: int,
         halt: bool = False,
 ) -> Iterator[str]:
-    states = tuple(map(chr, range(65, 65 + state_count)))
-    colors = tuple(map(str, range(color_count)))
-
-    actions = filter(
+    yield from filter(
         (
             lambda action: action[2] != HALT or action == '1R_'
             if halt else
             lambda action: action
         ),
         (
-            ''.join(prod) for prod
-            in product(
-                colors,
+            ''.join(prod)
+            for prod in product(
+                tuple(map(str, range(colors))),
                 SHIFTS,
-                states + ((HALT,) if halt else ()))
+                tuple(map(chr, range(65, 65 + states)))
+                + ((HALT,) if halt else ())
+            )
         )
     )
-
-    progs = (
-        '  '.join(state)
-        for state in
-        product(
-            (
-                ' '.join(state)
-                for state in
-                product(actions, repeat = color_count)
-            ),
-            repeat = state_count))
-
-    for prog in progs:
-        if max(colors) not in prog:
-            continue
-
-        if prog[:3] != '1RB':
-            continue
-
-        if halt and prog.count(HALT) != 1:
-            continue
-
-        yield prog
-
-
-
-def b0_halt(colors: int) -> str:
-    return '  '.join([
-        ' '.join(('...' for _ in range(colors))),
-        '.._',
-    ])
-
-
-def r_on_0(states: int, colors: int) -> str:
-    return '^' + '  '.join(
-        ' '.join(['.R.'] + ['...' for _ in range(colors - 1)])
-        for _ in range(states)
-    )
-
-
-def reject(
-        rejects: Rejects,
-        states: int,
-        colors: int,
-        halt: bool = False,
-) -> Callable[[str], bool]:
-    rejects = list(rejects)
-
-    if any(isinstance(rej, str) for rej in rejects):
-        rejects = [re.compile(regex) for regex in rejects]
-
-    rejects.insert(0, re.compile(r_on_0(states, colors)))
-
-    if halt:
-        rejects.insert(0, re.compile(b0_halt(colors)))
-
-    def reject_prog(prog: str) -> bool:
-        return any(
-            re.compile(regex).match(prog)
-            if isinstance(regex, str) else
-            regex.match(prog)
-            for regex in rejects
-        )
-
-    return reject_prog
 
 
 def yield_programs(
@@ -102,11 +36,27 @@ def yield_programs(
         halt: bool,
         rejects: Rejects | None = None,
 ) -> Iterator[str]:
-    if rejects is None:
-        rejects = []
-
-    reject_prog = reject(rejects, states, colors, halt)
-
-    for prog in yield_all_programs(states, colors, halt):
-        if not reject_prog(prog):
-            yield prog
+    yield from filter(
+        lambda prog: not any(
+            re.compile(regex).match(prog)
+            if isinstance(regex, str) else
+            regex.match(prog)
+            for regex in rejects or []
+        ),
+        (
+            prog for prog in (
+                '  '.join(state)
+                for state in product(
+                    (
+                        ' '.join(state)
+                        for state in
+                        product(
+                            yield_actions(states, colors, halt),
+                            repeat = colors)
+                    ),
+                    repeat = states)
+            ) if (
+                prog[:3] == '1RB'
+                and (not halt or prog.count(HALT) == 1))
+        )
+    )
