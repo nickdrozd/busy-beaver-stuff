bad54cc4bbfe39d8d82ff586f916fa68bd533cd7
Nick Drozd <nicholasdrozd@gmail.com>
2022-12-06 09:45:32 -0500

    Add surprise-in-a-box structured

---
+++ machines/structured/2315619.c
@@ -0,0 +1,74 @@
+#define PROGRAM "1RB 2LB 1LC  1LA 2RB 1RB  1R_ 2LA 0LC"
+
+#define TAPELEN 48
+
+#include "machine.h"
+
+int main(void)
+{
+ A:
+  switch (SCAN) {
+    case 0:
+      // A0
+      WRITE(1);
+      RIGHT;
+      goto B;
+
+    case 1:
+      // A1
+      WRITE(2);
+      LEFT;
+      goto B;
+
+    case 2:
+      // A2
+      WRITE(1);
+      LEFT;
+      goto C;
+  }
+
+ B:
+  switch (SCAN) {
+    case 0:
+      // B0
+      WRITE(1);
+      LEFT;
+      goto A;
+
+    case 1:
+      // B1
+      WRITE(2);
+      RIGHT;
+      goto B;
+
+    case 2:
+      // B2
+      WRITE(1);
+      RIGHT;
+      goto B;
+  }
+
+ C:
+  switch (SCAN) {
+    case 0:
+      // C0
+      WRITE(1);
+      RIGHT;
+      goto H;
+
+    case 1:
+      // C1
+      WRITE(2);
+      LEFT;
+      goto A;
+
+    case 2:
+      // C2
+      WRITE(0);
+      LEFT;
+      goto C;
+  }
+
+ H:
+  CHECK_STEPS;
+}
