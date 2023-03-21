d56e5d219061a8f8f3707a31851a62748201e0ac
Nick Drozd <nicholasdrozd@gmail.com>
2023-03-21 12:15:38 -0400

    Sweep away old tree gen stuff

--- tree_gen.py
+++ tree_gen.py
@@ -1,42 +1,6 @@
 from argparse import ArgumentParser
 
-from tm.program import Program
-from tm.utils import run_variations
 from generate.tree import run_tree_gen
-
-
-def prune_print(prog: str) -> None:
-    if not HALT and '.' in prog:
-        return
-
-    if COLORS == 2 and prog.count('0') < 2:
-        return
-
-    if not (program := Program(prog)).graph.is_strongly_connected:
-        return
-
-    if not program.graph.is_simple:
-        return
-
-    if not HALT and program.cant_spin_out:
-        return
-
-    run_and_print(prog)
-
-
-def print_complete(prog: str) -> None:
-    if len(set(Program(prog).used_states)) != STATES:
-        return
-
-    run_and_print(prog)
-
-
-def run_and_print(prog: str) -> None:
-    if any(machine.xlimit is None for machine in
-           run_variations(prog, 200)):
-        return
-
-    print(Program(prog).normalize())
 
 
 if __name__ == '__main__':
@@ -48,23 +12,13 @@ if __name__ == '__main__':
     parser.add_argument('--steps', type = int, default = 200)
 
     parser.add_argument('--halt', action = 'store_true')
-    parser.add_argument('--aggressive', action = 'store_true')
 
     args = parser.parse_args()
 
-    HALT   = args.halt
-    STEPS  = args.steps
-    COLORS = args.colors
-    STATES = args.states
-
     run_tree_gen(
-        states = STATES,
-        colors = COLORS,
-        halt   = HALT,
-        steps = STEPS,
-        output = (
-            prune_print
-            if args.aggressive else
-            print_complete
-        ),
+        states = args.states,
+        colors = args.colors,
+        halt   = args.halt,
+        steps  = args.steps,
+        output = print,
     )
