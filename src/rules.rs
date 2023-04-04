977addea26b9d2102c5ec0e5aed9ab1887672ffa
Nick Drozd <nicholasdrozd@gmail.com>
2023-04-04 14:43:26 -0400

    Revert "Start rules rewrite"
    This reverts commit 0d1b8bf03f8d358c5a6b5cbb8ca7e3a858ed4e39.

--- src/rules.rs
+++
@@ -1,6 +0,0 @@
-use pyo3::create_exception;
-use pyo3::exceptions::PyException;
-
-create_exception!(rules, UnknownRule, PyException);
-create_exception!(rules, InfiniteRule, PyException);
-create_exception!(rules, RuleLimit, PyException);
