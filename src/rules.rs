ab5b0d65f512a069d83708f244566a232112987d
Nick Drozd <nicholasdrozd@gmail.com>
2023-04-06 17:08:41 -0400

    Use BigInt

--- src/rules.rs
+++ src/rules.rs
@@ -1,3 +1,4 @@
+use num_bigint::BigInt;
 use pyo3::create_exception;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
@@ -6,7 +7,7 @@ create_exception!(rules, UnknownRule, PyException);
 create_exception!(rules, InfiniteRule, PyException);
 create_exception!(rules, RuleLimit, PyException);
 
-type Num = i128;
+type Num = BigInt;
 
 #[derive(Debug)]
 pub enum Op {
@@ -29,17 +30,17 @@ pub fn calculate_diff(cnt1: Num, cnt2: Num, cnt3: Num) -> PyResult<Option<Op>> {
         return Ok(None);
     }
 
-    let plus = cnt2 - cnt1;
-    if plus == cnt3 - cnt2 {
+    let plus = &cnt2 - &cnt1;
+    if plus == &cnt3 - &cnt2 {
         return Ok(Some(Op::Plus(plus)));
     }
 
-    let div = cnt2 / cnt1;
+    let div = &cnt2 / &cnt1;
 
-    if div == cnt3 / cnt2 {
-        let mod_ = cnt2 % cnt1;
+    if div == &cnt3 / &cnt2 {
+        let mod_ = &cnt2 % &cnt1;
 
-        if mod_ == cnt3 % cnt2 {
+        if mod_ == &cnt3 % &cnt2 {
             return Ok(Some(Op::Mult(div, mod_)));
         }
     }
