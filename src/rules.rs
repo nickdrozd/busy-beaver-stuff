1a07746d5848445f6b066f2569ee97e8a2f43acb
Nick Drozd <nicholasdrozd@gmail.com>
2023-04-07 17:15:55 -0400

    Use div_rem

--- src/rules.rs
+++ src/rules.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use num_bigint::BigInt;
+use num_integer::Integer;
 use pyo3::create_exception;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
@@ -14,7 +15,7 @@ type Num = BigInt;
 #[derive(Debug)]
 pub enum Op {
     Plus(Num),
-    Mult(Num, Num),
+    Mult((Num, Num)),
 }
 
 type Index = (usize, usize);
@@ -27,7 +28,7 @@ impl IntoPy<PyObject> for Op {
     fn into_py(self, py: Python) -> PyObject {
         match self {
             Self::Plus(val) => val.into_py(py),
-            Self::Mult(val1, val2) => (val1, val2).into_py(py),
+            Self::Mult(val) => val.into_py(py),
         }
     }
 }
@@ -44,14 +45,10 @@ pub fn calculate_diff(cnt1: Num, cnt2: Num, cnt3: Num) -> PyResult<Option<Op>> {
         return Ok(Some(Op::Plus(plus)));
     }
 
-    let div = &cnt2 / &cnt1;
+    let div_rem = cnt2.div_rem(&cnt1);
 
-    if div == &cnt3 / &cnt2 {
-        let mod_ = &cnt2 % &cnt1;
-
-        if mod_ == &cnt3 % &cnt2 {
-            return Ok(Some(Op::Mult(div, mod_)));
-        }
+    if div_rem == cnt3.div_rem(&cnt2) {
+        return Ok(Some(Op::Mult(div_rem)));
     }
 
     Err(UnknownRule::new_err(""))
@@ -92,7 +89,7 @@ pub fn make_rule(counts1: Counts, counts2: Counts, counts3: Counts) -> PyResult<
 
     let all_plus_positive = rule.values().all(|op| match op {
         Op::Plus(val) => val >= &BigInt::from(0),
-        Op::Mult(_, _) => true,
+        Op::Mult(_) => true,
     });
 
     if all_plus_positive {
