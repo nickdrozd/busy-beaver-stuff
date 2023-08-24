bbcb3a2359b132600a9dfd647be536698edf2f8e
Nick Drozd <nicholasdrozd@gmail.com>
2023-08-23 20:10:20 -0400

    Add number recursion error test

--- test/test_turing.py
+++ test/test_turing.py
@@ -860,6 +860,19 @@ class Fast(TuringTest):
                 show_number(self.machine.marks),
                 prog)
 
+    def test_number_recursion_error(self):
+        try:
+            self.run_bb(
+                "1RB 2LA 0RB 0LA  1LA 3RA 1RA ...",
+                sim_lim = 20239,
+                opt_macro = 108,
+                backsym = 1,
+            )
+        except RecursionError:
+            pass
+        else:
+            assert False
+
 
 class Slow(TuringTest):
     @skip('')
