951edc81f7653c420699ca3a1d73ece9057fb17c
Nick Drozd <nicholasdrozd@gmail.com>
2023-12-11 10:19:39 -0500

    Move holdouts to perf main

--- perf/__init__.py
+++ perf/__init__.py
@@ -6,10 +6,21 @@ import sys
 
 from typing import TYPE_CHECKING
 
+from test.utils import read_progs
+
 if TYPE_CHECKING:
     from collections.abc import Callable
 
     Null = Callable[[], None]
+
+
+HOLDOUTS = (
+    read_progs('holdouts_32q')
+    | read_progs('holdouts_23q')
+    | read_progs('holdouts_42h')
+    | read_progs('holdouts_42q')
+    | read_progs('holdouts_24h')
+)
 
 
 def profile(function: Null) -> Null:
--- perf/reason.py
+++ perf/reason.py
@@ -1,20 +1,10 @@
-from test.utils import read_progs
+from perf import profile, HOLDOUTS
 from tm.reason import BackwardReasoner
-from perf import profile
-
-
-PROGS = (
-    read_progs('holdouts_32q')
-    | read_progs('holdouts_23q')
-    | read_progs('holdouts_42h')
-    | read_progs('holdouts_42q')
-    | read_progs('holdouts_24h')
-)
 
 
 @profile
 def main() -> None:
-    for prog in PROGS:
+    for prog in HOLDOUTS:
         print(prog)
 
         reasoner = BackwardReasoner(prog)
