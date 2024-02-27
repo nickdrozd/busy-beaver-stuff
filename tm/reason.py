b3270e2d599c9f8d04e67360a1afb4d477d6d699
Nick Drozd <nicholasdrozd@gmail.com>
2024-02-27 10:58:11 -0500

    Cut branch_back

--- tm/reason.py
+++ tm/reason.py
@@ -8,7 +8,7 @@ from tm.tape import HeadTape
 from tm.rust_stuff import BackstepMachine
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Iterator
+    from collections.abc import Callable
 
     from tm.program import State, Slot
 
@@ -76,54 +76,42 @@ class BackwardReasoner(Program):
 
             # print(step, state, tape)
 
-            for config in self.branch_back(
-                    step, state, tape, machine, get_result):
-                configs.append(config)
+            for entry in sorted(self.graph.entry_points[state]):
+                for _, instr in self.get_switch(entry).items():
+                    if instr is None:
+                        continue
+
+                    _, shift, trans = instr
+
+                    if trans != state:
+                        continue
+
+                    for color in self.colors:
+                        machine.backstep_run(
+                            sim_lim = step + 1,
+                            init_tape = tape.to_tuples(),
+                            state = entry,
+                            shift = shift,
+                            color = color,
+                        )
+
+                        if not (result := get_result(machine)):
+                            continue
+
+                        if abs(result - step) > 1:
+                            continue
+
+                        yield_tape = tape.copy()
+
+                        yield_tape.backstep(shift, color)
+
+                        configs.append((
+                            step + 1,
+                            entry,
+                            yield_tape,
+                        ))
 
         return False  # no-cover
-
-    def branch_back(
-            self,
-            step: int,
-            state: State,
-            tape: HeadTape,
-            machine: BackstepMachine,
-            get_result: GetResult,
-    ) -> Iterator[Config]:
-        for entry in sorted(self.graph.entry_points[state]):
-            for _, instr in self.get_switch(entry).items():
-                if instr is None:
-                    continue
-
-                _, shift, trans = instr
-
-                if trans != state:
-                    continue
-
-                for color in self.colors:
-                    machine.backstep_run(
-                        sim_lim = step + 1,
-                        init_tape = tape.to_tuples(),
-                        state = entry,
-                        shift = shift,
-                        color = color,
-                    )
-
-                    if not (result := get_result(machine)):
-                        continue
-
-                    if abs(result - step) > 1:
-                        continue
-
-                    yield_tape = tape.copy()
-
-                    yield_tape.backstep(shift, color)
-
-                    yield (
-                        step + 1,
-                        entry,
-                        yield_tape,
-                    )
 
 ########################################
 
