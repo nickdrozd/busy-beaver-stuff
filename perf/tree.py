17a8c23c7952ccb97e6d876627206a2826cab165
Nick Drozd <nicholasdrozd@gmail.com>
2024-03-22 14:44:13 -0400

    Run tree perf for more params

--- perf/tree.py
+++ perf/tree.py
@@ -1,19 +1,26 @@
 from perf import profile
 from tm.tree import worker, prep_branches
 
+PROG_PARAMS = (
+    (2, 2, 0),
+    (3, 2, 0),
+    (2, 3, 0),
+    (4, 2, 1),
+)
 
 @profile
 def main() -> None:
-    worker(
-        steps = 100,
-        halt = False,
-        output = print,
-        stack = prep_branches(
-            states = 2,
-            colors = 3,
-            halt = False,
-        ),
-    )
+    for states, colors, halt in PROG_PARAMS:
+        worker(
+            steps = 100,
+            halt = bool(halt),
+            output = print,
+            stack = prep_branches(
+                states = states,
+                colors = colors,
+                halt = bool(halt),
+            ),
+        )
 
 
 if __name__ == '__main__':
