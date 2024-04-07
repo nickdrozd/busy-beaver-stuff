9493d334dfd21406f6c5628ea59311b291ff4cd5
Nick Drozd <nicholasdrozd@gmail.com>
2024-04-07 16:11:22 -0400

    Use repr for enum tests

--- test/test_tape.py
+++ test/test_tape.py
@@ -565,12 +565,14 @@ class TestEnum(TestCase):
 
     def assert_tape(
             self,
-            lspan: list[tuple[int, int]],
-            scan: Color,
-            rspan: list[tuple[int, int]],
+            tape: str,
             offsets: tuple[int, int],
             edges: tuple[int, int],
     ):
+        self.assertEqual(
+            tape,
+            str(self.tape.tape))
+
         self.assertEqual(
             offsets,
             self.tape.offsets)
@@ -578,14 +580,6 @@ class TestEnum(TestCase):
         self.assertEqual(
             tuple(map(bool, edges)),
             self.tape.edges)
-
-        self.assertEqual(
-            (lspan, scan, rspan),
-            (
-                [(blk.color,blk.count) for blk in self.tape.tape.lspan],
-                self.tape.tape.scan,
-                [(blk.color,blk.count) for blk in self.tape.tape.rspan],
-            ))
 
     def test_offsets_1(self):
         # 1RB 2LA 1RA 2LB 2LA  0LA 2RB 3RB 4RA ...
@@ -605,19 +599,19 @@ class TestEnum(TestCase):
         self.step(0, 0, 0)  # B0
 
         self.assert_tape(
-            [(1, 10), (4, 1), (3, 11), (2, 1)], 1, [],
+            "2^1 3^11 4^1 1^10 [1]",
             (1, 0), (0, 0))
 
         self.step(0, 2, 1)  # A1
 
         self.assert_tape(
-            [(3, 11), (2, 1)], 4, [(2, 11)],
+            "2^1 3^11 [4] 2^11",
             (2, 0), (0, 0))
 
         self.step(0, 2, 1)  # A4
 
         self.assert_tape(
-            [(3, 10), (2, 1)], 3, [(2, 12)],
+            "2^1 3^10 [3] 2^12",
             (3, 0), (0, 0))
 
         self.step(0, 2, 0)  # A3
@@ -626,7 +620,7 @@ class TestEnum(TestCase):
         self.step(1, 1, 0)  # A0
 
         self.assert_tape(
-            [(1, 14), (4, 1), (3, 9), (2, 1)], 0, [],
+            "2^1 3^9 4^1 1^14 [0]",
             (3, 0), (0, 1))
 
     def test_offsets_2(self):
@@ -644,20 +638,20 @@ class TestEnum(TestCase):
         self.step(0, 5, 0)
 
         self.assert_tape(
-            [(2, 414422564), (3, 6)], 2, [(5, 1)],
+            "3^6 2^414422564 [2] 5^1",
             (1, 0), (0, 0))
 
         self.step(0, 5, 1)
 
         self.assert_tape(
-            [(3, 5)], 3, [(5, 414422566)],
+            "3^5 [3] 5^414422566",
             (2, 0), (0, 0))
 
         self.step(1, 2, 0)
         self.step(1, 2, 1)
 
         self.assert_tape(
-            [(2, 414422567), (3, 5)], 0, [],
+            "3^5 2^414422567 [0]",
             (2, 0), (0, 1))
 
     def test_offsets_3(self):
@@ -668,7 +662,7 @@ class TestEnum(TestCase):
         self.step(0, 1, 0)
 
         self.assert_tape(
-            [(3, 8)], 3, [(1, 11)],
+            "3^8 [3] 1^11",
             (1, 1), (0, 0))
 
     def test_edges_1(self):
@@ -677,7 +671,7 @@ class TestEnum(TestCase):
         self.step(0, 1, 0)
 
         self.assert_tape(
-            [], 0, [(1, 1)],
+            "[0] 1^1",
             (0, 0), (1, 0))
 
     def test_edges_2(self):
@@ -686,5 +680,5 @@ class TestEnum(TestCase):
         self.step(0, 2, 1)
 
         self.assert_tape(
-            [], 0, [(2, 4)],
+            "[0] 2^4",
             (1, 0), (1, 0))
