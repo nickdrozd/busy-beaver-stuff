90bd47dacf814f3db4da365310902d48b32473ea
Nick Drozd <nicholasdrozd@gmail.com>
2024-10-25 16:51:27 -0400

    Check loose lr against holdouts

--- test/test_holdouts.py
+++ test/test_holdouts.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
 
 from test.utils import read_holdouts
+from test.lin_rec import run_loose_linrec_machine
 
 from tm.machine import quick_term_or_rec
 
+
+STEPS = 1_000
 
 HOLDOUT_COUNTS = {
     '32q': 3,
@@ -14,6 +17,12 @@ HOLDOUT_COUNTS = {
 }
 
 TOTAL_HOLDOUTS = 883
+
+LR_FALSE_POSIIVES = {
+    "1RB 0RB  0LB 1RC  1LD 0RC  1LB 1LA",
+    "1RB 0RB  0LC 1RD  1LC 1LA  1LB 0RD",
+    "1RB 1LD  1LC 1RB  0RC 1LA  0RD 0LC",
+}
 
 
 class Holdouts(TestCase):
@@ -35,5 +44,14 @@ class Holdouts(TestCase):
 
         for prog in holdouts:
             self.assertFalse(
-                quick_term_or_rec(prog, 1_000),
+                quick_term_or_rec(prog, STEPS),
                 prog)
+
+            if prog in LR_FALSE_POSIIVES:
+                self.assertIsNotNone(
+                    run_loose_linrec_machine(prog, 300).infrul)
+
+                continue
+
+            self.assertIsNotNone(
+                run_loose_linrec_machine(prog, STEPS).xlimit)
