e66d1da03818571d068e1a1bc274dc5cbc4248ad
Nick Drozd <nicholasdrozd@gmail.com>
2025-02-04 09:25:35 -0500

    Sort reason imports

--- tm/reason.py
+++ tm/reason.py
@@ -1,22 +1,12 @@
-# ruff: noqa: F401
+# ruff: noqa: F401, I001
 from tm.rust_stuff import (
     BackwardResult,
-)
-from tm.rust_stuff import (
-    py_cant_blank as cant_blank,
-)
-from tm.rust_stuff import (
+
     py_cant_halt as cant_halt,
-)
-from tm.rust_stuff import (
+    py_cant_blank as cant_blank,
     py_cant_spin_out as cant_spin_out,
-)
-from tm.rust_stuff import (
-    py_segment_cant_blank as segment_cant_blank,
-)
-from tm.rust_stuff import (
+
     py_segment_cant_halt as segment_cant_halt,
-)
-from tm.rust_stuff import (
+    py_segment_cant_blank as segment_cant_blank,
     py_segment_cant_spin_out as segment_cant_spin_out,
 )
