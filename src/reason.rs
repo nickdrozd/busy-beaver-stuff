8dd1c5ed9ae93bd9b2c65f305a40da2740fa94da
Nick Drozd <nicholasdrozd@gmail.com>
2025-02-13 10:01:21 -0500

    Add indef assertions to linrec check

--- src/reason.rs
+++ src/reason.rs
@@ -507,9 +507,7 @@ impl Config {
     }
 
     fn lin_rec(&self) -> bool {
-        if self.tape.has_indef() {
-            return false;
-        }
+        assert!(!self.tape.has_indef());
 
         let head = self.tape.head();
         let mut leftmost = head;
@@ -520,6 +518,8 @@ impl Config {
         #[expect(clippy::assigning_clones)]
         while let Some(config) = current {
             let pos = config.tape.head();
+
+            assert!(!config.tape.has_indef());
 
             if pos < leftmost {
                 leftmost = pos;
