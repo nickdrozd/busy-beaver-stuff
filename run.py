af670dc30fc0ba3705b3fb17ffa09859732b2159
Nick Drozd <nicholasdrozd@gmail.com>
2025-03-09 12:31:48 -0400

    Add run cli

--- run.py
+++ run.py
@@ -1,27 +1,53 @@
+import argparse
 import sys
 
 from tm.machine import Machine
 
-PRINT = 1
-STEPS = 10 ** 10
-BACKS = 0
 
-def main() -> None:
-    for i, prog in enumerate(sys.stdin):
-        program = prog.strip()
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
 
-        machine = Machine(
-            program,
-            opt_macro = 8_000,
-            backsym = BACKS or None,
-        ).run(
-            sim_lim = STEPS,
-            watch_tape = bool(PRINT),
-        )
+    parser.add_argument(
+        "-p", "--no-print",
+        action = "store_false",
+        dest = "print",
+        default = True,
+    )
 
-        print(f'{i} | {machine}')
+    parser.add_argument(
+        "-s", "--steps",
+        type = int,
+        default = 10 ** 6,
+    )
+
+    parser.add_argument(
+        "-b", "--backsym",
+        type = int,
+        default = None,
+    )
+
+    parser.add_argument(
+        "-m", "--macro",
+        type = int,
+        default = 8_000,
+    )
+
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
     sys.set_int_max_str_digits(100_000)
 
-    main()
+    args = parse_args()
+
+    for i, prog in enumerate(p.strip() for p in sys.stdin):
+        machine = Machine(
+            prog,
+            opt_macro = args.macro,
+            backsym = args.backsym,
+        ).run(
+            sim_lim = args.steps,
+            watch_tape = args.print,
+        )
+
+        print(f"{i} | {machine}")
