724e8cc65f83a1c4bcf74c8046e8fbb3baa68020
Nick Drozd <nicholasdrozd@gmail.com>
2025-03-14 11:59:40 -0400

    Move tree stub

--- tm/rust_stuff.pyi
+++ tm/rust_stuff.pyi
@@ -117,6 +117,14 @@ type Rule = dict[Index, Op]
 
 def py_opt_block(prog: str, steps: int) -> int: ...
 
+## tree ################################
+
+def tree_progs(
+        params: Params,
+        halt: bool,
+        sim_lim: int,
+) -> list[str]: ...
+
 ## reason ##############################
 
 def py_is_connected(prog: str, states: int) -> bool: ...
@@ -166,11 +174,3 @@ class SegmentResult:
 def py_segment_cant_halt(prog: str, segs: int) -> SegmentResult: ...
 def py_segment_cant_blank(prog: str, segs: int) -> SegmentResult: ...
 def py_segment_cant_spin_out(prog: str, segs: int) -> SegmentResult: ...
-
-## tree ################################
-
-def tree_progs(
-        params: Params,
-        halt: bool,
-        sim_lim: int,
-) -> list[str]: ...
