bbe912d05684b0d4ecc57557e3552601ea5f5157
Nick Drozd <nicholasdrozd@gmail.com>
2025-04-29 17:37:47 -0400

    Add blocks test

--- test/test_macro.py
+++ test/test_macro.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from tm.macro import make_macro
+from tm.macro import make_macro, opt_block
 
 MACROS = {
     ("0RB 0LC  1LA 1RB  1RD 0RE  1LC 1LA  ... 0LD", 2): {
@@ -20,3 +20,21 @@ class TestMacro(TestCase):
 
             for slot, instr in instrs.items():
                 self.assertEqual(macro[slot], instr)
+
+
+BLOCKS = {
+    "1RB 0LC 0LC  2LA 0RA 1RB  1LA 2LB ...": {
+        500: 8,
+        1000: 1,
+        2000: 45,
+    },
+}
+
+
+class TestBlocks(TestCase):
+    def test_blocks(self):
+        for prog, block_steps in BLOCKS.items():
+            for steps, blocks in block_steps.items():
+                self.assertEqual(
+                    blocks,
+                    opt_block(prog, steps))
