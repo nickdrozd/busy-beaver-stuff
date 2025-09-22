1a6b3f55247b22107b9afd6e836ad263e628b5f5
Nick Drozd <nicholasdrozd@gmail.com>
2025-09-22 12:08:39 -0400

    Rewrite branch script

--- branch.py
+++ branch.py
@@ -1,12 +1,49 @@
 import sys
+from itertools import product
 
-from test.test_turing import branch_last
 from tm.machine import Machine
+from tm.macro import show_comp
+from tm.parse import tcompile
+from tools import get_params
 
 if __name__ == '__main__':
     for prog in sys.stdin:
-        for i, branch in enumerate(branch_last(prog)):
-            machine = Machine(branch, opt_macro = 5_000).run(10_000)
+        prog = prog.strip()  # noqa: PLW2901
 
-            if machine.xlimit is None:
-                print(i, machine)
+        print(f'trunk: {prog}')
+
+        todo: list[str] = [prog]
+
+        while todo:  # pylint: disable = while-used
+            branch = todo.pop()
+
+            machine = Machine(
+                branch,
+                opt_macro = 5_000,
+            ).run(10_000)
+
+            if (undfnd := machine.undfnd) is None:
+                if not machine.infrul and not machine.xlimit:
+                    print(machine)
+
+                continue
+
+            print(f'undfnd: {branch}')
+
+            _, slot = undfnd
+
+            states, colors = get_params(branch)
+            comp = tcompile(branch)
+
+            instrs = product(
+                range(colors),
+                (True, False),
+                range(states),
+            )
+
+            for instr in instrs:
+                ext = show_comp(comp | { slot : instr })
+
+                print(f'branch: {ext}')
+
+                todo.append(ext)
